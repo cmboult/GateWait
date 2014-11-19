470a3123d211704a15d4c258e17526e257e5db64
Christian Boult <christian.boult@gmail.com>
2014-11-19 23:37:38 +0000

    Added GCM XMPP Smack Server to project

--- src/main/java/AppController.java
+++ src/main/java/AppController.java
@@ -1,3 +1,6 @@
+import org.jivesoftware.smack.XMPPException;
+
+import server.GateWaitGCMServer;
 import flightawareapi.APIScheduler;
 
 
@@ -10,6 +13,17 @@ public class AppController {
 		
 		APIScheduler scheduler = new APIScheduler(2);
 		scheduler.schedule();
+		
+		final String userName = "201748315501" + "@gcm.googleapis.com";
+	    final String password = "AIzaSyC5YZGgZZPo7KHmHbWetrFmtWrkFYIMRkI";
+
+	    GateWaitGCMServer client = new GateWaitGCMServer();
+
+	    try {
+	      client.connect(userName, password);
+	    } catch (XMPPException e) {
+	      e.printStackTrace();
+	    }
 
 	}
 
